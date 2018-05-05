65eda905c80da8ec8725f176a4e47b2e5d74fef2
Serina Ko <34416116+crushoncode@users.noreply.github.com>
2018-05-06 02:28:03 +1000

    Add Pages controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+
+    before_action :authenticate_user!
+
+    def after_sign_in_path_for(user)
+        if user.profile.nil?
+            new_profile_path
+        else
+            root_path
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  get 'pages/home'
   root 'pages#home'
 
   devise_for :users
