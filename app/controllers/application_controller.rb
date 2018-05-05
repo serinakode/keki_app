6e7008d77caba7139ee31f57101f33a550a60920
Serina Ko <34416116+crushoncode@users.noreply.github.com>
2018-05-06 09:32:32 +1000

    add navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,11 @@ class ApplicationController < ActionController::Base
 
     before_action :authenticate_user!
 
-    def after_sign_in_path_for(user)
-        if user.profile.nil?
-            new_profile_path
-        else
-            root_path
-        end
-    end
+#     def after_sign_in_path_for(user)
+#         if user.profile.nil?
+#             new_profile_path
+#         else
+#             root_path
+#         end
+#     end
 end
