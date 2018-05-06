6fc8b059acb509dffe856905c2f11313a0cc9a28
Serina Ko <34416116+crushoncode@users.noreply.github.com>
2018-05-06 16:06:45 +1000

    add authentication

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,6 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
 
-    # before_action :authenticate_user!
-
 #     def after_sign_in_path_for(user)
 #         if user.profile.nil?
 #             redirect_to new_profile_path
