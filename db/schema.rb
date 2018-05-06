3b58dbf05693850c50c41aa6864ef0f0018e40bd
Serina Ko <34416116+crushoncode@users.noreply.github.com>
2018-05-06 22:43:29 +1000

    add more colums to profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_06_051901) do
+ActiveRecord::Schema.define(version: 2018_05_06_115531) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,12 +32,18 @@ ActiveRecord::Schema.define(version: 2018_05_06_051901) do
     t.string "first_name"
     t.string "last_name"
     t.string "username"
-    t.integer "user_address"
     t.text "short_bio"
     t.text "image_data"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.string "street_address"
+    t.string "town_suburb"
+    t.string "city"
+    t.string "state"
+    t.integer "postcode"
+    t.decimal "latitude"
+    t.decimal "logitude"
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
