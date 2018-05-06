e7d6c97121af796eadcaeca25649173202f404fd
Serina Ko <34416116+crushoncode@users.noreply.github.com>
2018-05-06 14:48:52 +1000

    migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_05_231837) do
+ActiveRecord::Schema.define(version: 2018_05_06_043630) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.integer "seller_id"
+    t.string "product_name"
+    t.text "description"
+    t.integer "price"
+    t.text "image_data"
+    t.integer "quantity_available"
+    t.datetime "no_longer_selling"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "profiles", force: :cascade do |t|
     t.integer "user_id"
