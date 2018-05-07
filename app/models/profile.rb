941f3239763e507ebc8520e66a667c14ec5553ca
Serina Ko <34416116+crushoncode@users.noreply.github.com>
2018-05-07 15:22:51 +1000

    add geocoder to profile

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,4 +2,10 @@ class Profile < ApplicationRecord
     belongs_to :user
     include ImageUploader::Attachment.new(:image)
 
+    geocoded_by :full_street_address   # can also be an IP address
+    after_validation :geocode          # auto-fetch coordinates
+
+    def full_street_address
+        "#{street_address}, #{city}, #{postcode}, #{state}"
+    end
 end
