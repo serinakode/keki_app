2d1c50fd1452c3e7fddb99bb5595ca520edb2b13
Serina Ko <34416116+crushoncode@users.noreply.github.com>
2018-05-10 18:56:34 +1000

    try attr readers

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,6 +2,8 @@ class Profile < ApplicationRecord
     belongs_to :user
     include ImageUploader::Attachment.new(:image)
 
+    attr_reader :address, :city, :state, :postcode
+
     geocoded_by :full_street_address   # can also be an IP address
     after_validation :geocode          # auto-fetch coordinates
 
