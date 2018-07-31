5a9014837380f86865d7f555351421a68cb394e6
Serina Ko <34416116+crushoncode@users.noreply.github.com>
2018-07-31 20:43:54 +1000

    add address, latitude, and longitude

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_08_095225) do
+ActiveRecord::Schema.define(version: 2018_07_31_051545) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,9 @@ ActiveRecord::Schema.define(version: 2018_05_08_095225) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.string "address"
+    t.decimal "latitude"
+    t.decimal "longitude"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
